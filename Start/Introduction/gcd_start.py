5e8b95391d68ff18306a34c5d641ca2297460b20
Kaibiao Ruan <ruankaibiao@gmail.com>
2025-11-02 21:12:47 +0000

    gcd start

--- Start/Introduction/gcd_start.py
+++ Start/Introduction/gcd_start.py
@@ -4,7 +4,16 @@
 
 
 def gcd(a, b):
-    pass
+    if a >= b:
+        r = a % b
+        print(f"Computing GCD of {a} and {b}, remainder is {r}")
+        if r == 0:
+            return b
+        elif r == 1:
+            return None
+        return gcd(b, r)
+    else:
+        return gcd(b, a)
 
 
 # try out the function with a few examples
